a2f882bb45d10cd0f3effc548e6f5748f193dd0e
WANG Xuerui <git@xen0n.name>
2021-09-19 16:08:53 +0800

    we don't need pool labels

--- tcg/loongarch/tcg-target.h
+++ tcg/loongarch/tcg-target.h
@@ -170,7 +170,6 @@ void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 #ifdef CONFIG_SOFTMMU
 #define TCG_TARGET_NEED_LDST_LABELS
 #endif
-#define TCG_TARGET_NEED_POOL_LABELS
 
 #define TCG_TARGET_HAS_MEMORY_BSWAP 0
 
