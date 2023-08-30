a746e698a301bb633a5a70d0f9165d29a2039904
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-08-30 13:38:16 +0400

    ui/vc: move text console invalidate in helper
    This will allow to split the VC code in a separate unit more easily.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20230830093843.3531473-43-marcandre.lureau@redhat.com>

--- ui/console.c
+++ ui/console.c
@@ -2503,12 +2503,10 @@ static void vc_chr_set_echo(Chardev *chr, bool echo)
     drv->console->echo = echo;
 }
 
-static void text_console_update_cursor(void *opaque)
+int qemu_invalidate_text_consoles(void)
 {
     QemuConsole *s;
     int count = 0;
-
-    cursor_visible_phase = !cursor_visible_phase;
 
     QTAILQ_FOREACH(s, &consoles, next) {
         if (qemu_console_is_graphic(s) ||
@@ -2519,7 +2517,14 @@ static void text_console_update_cursor(void *opaque)
         graphic_hw_invalidate(s);
     }
 
-    if (count) {
+    return count;
+}
+
+static void text_console_update_cursor(void *opaque)
+{
+    cursor_visible_phase = !cursor_visible_phase;
+
+    if (qemu_invalidate_text_consoles()) {
         timer_mod(cursor_timer,
                   qemu_clock_get_ms(QEMU_CLOCK_REALTIME) + CONSOLE_CURSOR_PERIOD / 2);
     }
